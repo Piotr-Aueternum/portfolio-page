9d5eda9ba1070d29c8a6289c5074025f98b28c39
piotr-aueternum <piotr.aueternum@gmail.com>
2017-02-10 15:54:29 +0100

    Slide to anchor added, anchor detect added as fp-id

--- dist/assets/bundle.js
+++ dist/assets/bundle.js
@@ -196,14 +196,35 @@ class Fullpage {
     this.onLeave = onLeave;
     this.fullpage = document.getElementById(container);
     this.wrapperHeight = this.fullpage.clientHeight;
+    this.sections = this.fullpage.querySelectorAll(this.section);
     this.transform = 0;
     this.sectionHeight = 0;
+    this.index = 0;
+    this.id = window.location.hash.replace(/\?|#/, '');
+    this.initId = this.id;
     this.init();
+  }
+  slideTo() {
+    this.sections.forEach((sectionEl, key) => {
+      if (this.initId === sectionEl.dataset.fpId) {
+        this.transform = -key * this.sectionHeight;
+        this.index = key;
+        this.fullpage.style.transform = `translate3d(0, ${Math.floor(this.transform)}px, 0)`;
+      } else {
+        this.setHash();
+      }
+    });
+  }
+  setHash() {
+    this.hash = this.sections[this.index].dataset.fpId;
+    window.location.hash = this.hash;
+    this.id = window.location.hash.replace(/\?|#/, '');
   }
   init() {
     this.setupContainer();
     this.setSectionsHeight();
     this.slide();
+    this.slideTo();
   }
   slide() {
     this.time = Date.now();
@@ -220,11 +241,12 @@ class Fullpage {
           this.index += 1;
           this.transform -= this.sectionHeight;
         }
-        this.fullpage.style.transform = `translate3d(0, ${this.transform}px, 0)`;
+        this.fullpage.style.transform = `translate3d(0, ${Math.floor(this.transform)}px, 0)`;
         this.time = Date.now();
         if (this.onLeave && typeof this.onLeave === 'function') {
           this.onLeave(this.index);
         }
+        this.setHash();
       }
     });
   }
@@ -234,13 +256,16 @@ class Fullpage {
     this.fullpage.style.transitionDuration = `${this.delay}ms`;
     this.fullpage.style.transform = `translate3d(0, ${this.transform}px, 0)`;
   }
+  getSectionHeight() {
+    return this.wrapperHeight / this.sections.length;
+  }
   setSectionsHeight() {
     if (this.section) {
-      const sections = this.fullpage.querySelectorAll(this.section);
-      const sectionAmount = sections.length;
-      this.sectionHeight = this.wrapperHeight / sectionAmount;
-      for (let i = 0; i < sections.length; i += 1) {
-        sections[i].style.height = `${this.sectionHeight}px`;
+      this.sectionHeight = this.getSectionHeight();
+      const sectionHeight = this.getSectionHeight();
+      const sectionAmount = this.sections.length;
+      for (let i = 0; i < sectionAmount; i += 1) {
+        this.sections[i].style.height = `${Math.floor(sectionHeight)}px`;
       }
     } else {
       throw new TypeError('Section elements not found.');
