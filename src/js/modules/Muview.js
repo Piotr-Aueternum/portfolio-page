b93de9a413618ce400fad786169d448a94c865f8
piotr-aueternum <piotr.aueternum@gmail.com>
2017-02-16 16:59:38 +0100

    remove fullpage rename into Muview.js

--- src/js/modules/Muview.js
+++ src/js/modules/Muview.js
@@ -1,9 +1,9 @@
 import isFn from '../utilities/isFn';
 import debounce from '../utilities/debounce';
 
-export default class Fullpage {
+export default class Muview {
   constructor({
-    container = 'fullpage',
+    container = 'muview',
     section = '.section',
     delay = 700,
     onStart,
@@ -16,7 +16,7 @@ export default class Fullpage {
     this.onStart = onStart;
     this.onSlide = onSlide;
     this.onLeave = onLeave;
-    this.fullpage = document.getElementById(container);
+    this.muview = document.getElementById(container);
     this.transform = 0;
     this.index = 0;
     this.id = window.location.hash.replace(/\?|#/, '');
@@ -28,52 +28,52 @@ export default class Fullpage {
   }
   init() {
     document.body.classList.add(this.container);
-    this.fullpage.classList.add(`${this.container}__wrapper`);
-    this.fullpage.style.transitionDuration = `${this.delay}ms`;
+    this.muview.classList.add(`${this.container}__wrapper`);
+    this.muview.style.transitionDuration = `${this.delay}ms`;
     this.setSectionHeight();
-    this.updatePosition();
+    this.setTransform();
+  }
+  afterLoad() {
+    this.sections.forEach((sectionEl, key) => {
+      if (this.initId === sectionEl.dataset.mvId) {
+        this.index = key;
+      }
+    });
+  }
+  afterResize() {
+    const onResize = debounce(() => {
+      this.setSectionHeight();
+      this.setTransform();
+    }, this.delay);
+    window.addEventListener('resize', onResize);
   }
   setSectionHeight() {
-    this.sections = this.fullpage.querySelectorAll(this.section);
-    this.wrapperHeight = this.fullpage.clientHeight;
+    this.sections = this.muview.querySelectorAll(this.section);
+    this.wrapperHeight = this.muview.clientHeight;
     this.sectionHeight = window.innerHeight;
     for (let i = 0; i < this.sections.length; i += 1) {
       this.sections[i].style.height = `${Math.floor(this.sectionHeight)}px`;
     }
   }
-  afterResize() {
-    const onResize = debounce(() => {
-      this.setSectionHeight();
-      this.updatePosition();
-    }, this.delay);
-    window.addEventListener('resize', onResize);
-  }
-  updatePosition() {
+  setTransform() {
     this.transform = -this.index * this.sectionHeight;
-    this.fullpage.style.transform = `translate3d(0, ${Math.floor(this.transform)}px, 0)`;
-  }
-  afterLoad() {
-    this.sections.forEach((sectionEl, key) => {
-      if (this.initId === sectionEl.dataset.fpId) {
-        this.index = key;
-      }
-    });
+    this.muview.style.transform = `translate3d(0, ${Math.floor(this.transform)}px, 0)`;
   }
   setIndex() {
     this.sections.forEach((sectionEl, key) => {
-      if (this.id === sectionEl.dataset.fpId) {
+      if (this.id === sectionEl.dataset.mvId) {
         this.index = key;
       }
     });
   }
   setHash() {
-    if (this.sections[this.index].dataset.fpId) {
-      this.hash = this.sections[this.index].dataset.fpId;
+    if (this.sections[this.index].dataset.mvId) {
+      this.hash = this.sections[this.index].dataset.mvId;
       window.location.hash = this.hash;
       this.updateId();
     }
   }
-  changeIndex(value) {
+  changeIndexBy(value) {
     this.index += value;
   }
   updateId() {
@@ -83,20 +83,24 @@ export default class Fullpage {
     window.addEventListener('hashchange', () => {
       this.updateId();
       this.setIndex();
-      this.updatePosition();
+      this.setTransform();
     });
     this.time = Date.now();
     window.addEventListener('wheel', (e) => {
       this.direction = e.deltaY < 0 ? 'up' : 'down';
       if (((this.time + this.delay) - Date.now()) < 0) {
-        isFn(this.onStart(this.index, this.direction));
+        if (isFn(this.onStart)) {
+          this.onStart(this.index, this.direction);
+        }
         setTimeout(() => {
-          isFn(this.onLeave(this.index, this.direction));
+          if (isFn(this.onLeave)) {
+            this.onLeave(this.index, this.direction);
+          }
         }, this.delay);
         if (this.direction === 'up' && this.index > 0) {
-          this.changeIndex(-1);
+          this.changeIndexBy(-1);
         } else if (this.direction === 'down' && this.index < this.sections.length - 1) {
-          this.changeIndex(1);
+          this.changeIndexBy(1);
         }
         this.setHash();
         this.time = Date.now();
